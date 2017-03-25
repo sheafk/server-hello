3a7c040af689e736428429237328b31c9dc7380b
sheafk <sheafureyking@gmail.com>
2017-03-24 20:22:44 -0400

    Removed /

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,11 +1,10 @@
-import Foundation
 import Vapor
 
 print("My serving is starting")
 
 let server = Droplet()
 
-server.get("/hello") { request in
+server.get("hello") { request in
     return "Hello Shea"
 }
 
