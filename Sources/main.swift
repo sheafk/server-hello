bb4dd0503dcbadf0caca2b7558731198d882a7f9
sheafk <sheafureyking@gmail.com>
2017-03-24 21:26:11 -0400

    changed run to serve

--- Sources/main.swift
+++ Sources/main.swift
@@ -8,5 +8,16 @@ drop.get("/") { request in
     return "Hello Shea"
 }
 
+drop.get("/name",":name") { request in
+    if let name = request.parameters["name"].string {
+        return "Hello \(name)!"
+    }
+    return "Error retrieving parameters."
+}
+
+drop.get("/view") { request in
+    return try drop.view("view.html")
+}
+
 //serve()
-drop.run()
+drop.serve()
