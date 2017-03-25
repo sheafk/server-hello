9f4216feb38058f0e71741337e86195a717b87e4
sheafk <sheafureyking@gmail.com>
2017-03-24 21:46:48 -0400

    Using Vapor code

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,20 +4,8 @@ print("My serving is starting")
 
 let drop = Droplet()
 
-drop.get("/") { request in
+drop.get("/hello") { _ in
     return "Hello Shea"
 }
 
-drop.get("/name",":name") { request in
-    if let name = request.parameters["name"].string {
-        return "Hello \(name)!"
-    }
-    return "Error retrieving parameters."
-}
-
-drop.get("/view") { request in
-    return try drop.view("view.html")
-}
-
-//serve()
 drop.run()
